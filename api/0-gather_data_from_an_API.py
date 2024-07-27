c41c1fe7c6e93fcf574b8f19f9343db9a26a16ed
root <root@OSCAR-J-ALFARO-M>
2024-07-27 15:17:51 -0500

    subiendo task1

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,53 +1,24 @@
 #!/usr/bin/python3
-"""
-Gather data from an API
-"""
+"""0th task file"""
+
 import requests
-import sys
-
-
-def gather_data(employee_id):
-    """Fetch and display employee TODO list progress."""
-    api_uri = "https://jsonplaceholder.typicode.com/"
-
-    try:
-        # Fetch user data
-        user_response = requests.get(f"{api_uri}/users/{employee_id}")
-        user_response.raise_for_status()  # Check for HTTP errors
-        user_data = user_response.json()
-        emp_name = user_data.get("name")
-
-        # Fetch TODO data
-        todos_response = requests.get(f"{api_uri}/todos?userId={employee_id}")
-        todos_response.raise_for_status()  # Check for HTTP errors
-        todos_data = todos_response.json()
-
-        total_tasks = len(todos_data)
-        done_tasks = [task for task in todos_data if task.get("completed")]
-        total_tasks_done = len(done_tasks)
-
-        # Print results
-        print(
-            f"Employee {emp_name} is done with tasks("
-            f"{total_tasks_done}/{total_tasks}):"
-        )
-        for task in done_tasks:
-            print(f"\t {task.get('title')}")
-
-    except requests.RequestException as e:
-        print(f"Error fetching data from API: {e}")
-    except ValueError as e:
-        print(f"Invalid data received: {e}")
-
+from sys import argv
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
-
-    try:
-        emp_id = int(sys.argv[1])
-        gather_data(emp_id)
-    except ValueError:
-        print("Error: employee_id should be an integer")
-        sys.exit(1)
+    empID = argv[1]
+    employeeData = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{empID}')
+    employeeTODOs = requests.get(
+        f'https://jsonplaceholder.typicode.com/users/{empID}/todos').json()
+    employeeName = employeeData.json()["name"]
+    completedTasks = []
+    countCompletedTasks = 0
+    totalTasks = len(employeeTODOs)
+    for todo in employeeTODOs:
+        if todo["completed"] is True:
+            countCompletedTasks += 1
+            completedTasks.append(todo["title"])
+    print("Employee {} is done with tasks({}/{}):".format(
+        employeeName, countCompletedTasks, totalTasks))
+    for task in completedTasks:
+        print("\t{}".format(task))
